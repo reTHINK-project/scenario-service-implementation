92e8f297d9699dfd7a386121ead4491de5cb3841
pbz <pbz@posteo.de>
2016-04-27 23:37:32 +0200

    lwm2m-raspberry: Added promise-wrapper for lwm2m object creation

--- nodejs_rethink_module_lwm2m/lwm2m-raspberry/dist/Util.js
+++ nodejs_rethink_module_lwm2m/lwm2m-raspberry/dist/Util.js
@@ -8,15 +8,37 @@ Object.defineProperty(exports, "__esModule", {
 });
 var util = {};
 
+//Methods wrapped in promises
+
+util.createClientObject = function (client, objectUri) {
+    return new Promise(function (resolve, reject) {
+        if (typeof client === "undefined" || typeof objectUri === "undefined") {
+            reject(new Error("util.createClientObject: Invalid parameters!"));
+        } else {
+            client.registry.create(objectUri, function (error) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+        }
+    });
+};
+
 util.setClientResource = function (client, objectUri, resourceId, value) {
     return new Promise(function (resolve, reject) {
-        client.registry.setResource(objectUri, resourceId, value, function (error, result) {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(result);
-            }
-        });
+        if (typeof client === "undefined" || typeof objectUri === "undefined" || typeof resourceId === "undefined" || typeof value === "undefined") {
+            reject(new Error("util.setClientResource: Invalid parameters!"));
+        } else {
+            client.registry.setResource(objectUri, resourceId, value, function (error, result) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        }
     });
 };
 
--- nodejs_rethink_module_lwm2m/lwm2m-raspberry/src/Util.js
+++ nodejs_rethink_module_lwm2m/lwm2m-raspberry/src/Util.js
@@ -5,16 +5,41 @@
 
 var util = {};
 
+//Methods wrapped in promises
+
+util.createClientObject = function (client, objectUri) {
+    return new Promise((resolve, reject) => {
+        if (typeof client === "undefined" || typeof objectUri === "undefined") {
+            reject(new Error("util.createClientObject: Invalid parameters!"));
+        }
+        else {
+            client.registry.create(objectUri, (error) => {
+                if (error) {
+                    reject(error);
+                }
+                else {
+                    resolve();
+                }
+            })
+        }
+    });
+};
+
 util.setClientResource = function (client, objectUri, resourceId, value) {
     return new Promise((resolve, reject) => {
-        client.registry.setResource(objectUri, resourceId, value, (error, result) => {
-            if (error) {
-                reject(error);
-            }
-            else {
-                resolve(result);
-            }
-        });
+        if (typeof client === "undefined" || typeof objectUri === "undefined" || typeof resourceId === "undefined" || typeof value === "undefined") {
+            reject(new Error("util.setClientResource: Invalid parameters!"));
+        }
+        else {
+            client.registry.setResource(objectUri, resourceId, value, (error, result) => {
+                if (error) {
+                    reject(error);
+                }
+                else {
+                    resolve(result);
+                }
+            });
+        }
     });
 };
 
